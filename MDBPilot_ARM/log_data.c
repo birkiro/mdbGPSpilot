bdfa8ca887faf3b9da4a9acdba953fabdcaabad2
ilya90 <ilja.fursov@gmail.com>
2013-05-14 14:39:54 +0200

    logdata functions added. Printing vx to terminal.

---
+++ MDBPilot_ARM/log_data.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include "log_data.h"
+#include "states.h"
+
+void logdata(void){
+
+	printf("%f\n",navdata_unpacked.navdata_demo.vx);
+	//fprintf(f, "VX: %s\n", "lknlrk");
+
+}
---
+++ MDBPilot_ARM/log_data.h
@@ -0,0 +1,16 @@
+/*
+ * log_data.h
+ *
+ *  Created on: May 14, 2013
+ *      Author: ilya
+ */
+
+#ifndef LOG_DATA_H_
+#define LOG_DATA_H_
+
+FILE *f;
+
+void logdata(void);
+
+
+#endif /* LOG_DATA_H_ */
