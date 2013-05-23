62b262c72798ac327fc29cb5d49fd8783bbcdee8
ilya90 <ilja.fursov@gmail.com>
2013-05-23 13:11:50 +0200

    Speed limits changed. Go to homeposition added.ALTI function used differently.

--- Arduino/mdbPilotbasic/mdbPilotbasic.ino
+++ Arduino/mdbPilotbasic/mdbPilotbasic.ino
@@ -11,7 +11,7 @@ int flag_flying = 0;
 int  fix_led = 13;
 float flat, flon;
 unsigned long age;
-
+int wp_reached = 0;
 void setup()
 {
   bool fixed = false;
@@ -47,7 +47,7 @@ void loop()
     {
       drone_sethomepos();  // Set Home Position, set global variables lat_home, lon_home
 //      drone_setwp(54.900700, 9.807400);   // Set Waypoint Position (longitude, latitude)
-      drone_setwp(54.913328, 9.779755);   // Set Waypoint Position (longitude, latitude)
+      drone_setwp(54.900500, 9.807990);   // Set Waypoint Position (longitude, latitude)
       Serial.print("Distance to wp:");
       Serial.println(calculate_distance(lat_home, lon_home, lat_wp, lon_wp));
       if(calculate_distance(lat_home, lon_home, lat_wp, lon_wp) > 500)
@@ -55,7 +55,7 @@ void loop()
         Serial.print("$ECHO,#Distance too great! Flight Aborted! \n");
         Serial.print("Distance to wp:");
         Serial.println(calculate_distance(lat_home, lon_home, lat_wp, lon_wp));
-        //while(1) delay(10000);  // Never fly, abort mission
+        while(1) delay(10000);  // Never fly, abort mission
       }
       drone_takeoff();
       Serial.print("$ECHO,#We Have Liftoff! \n");
@@ -68,11 +68,15 @@ void loop()
     head_to_target(angle);
     drone_move(distance_to_wp);
     
-    if (distance_to_wp < 5) 
+    if (distance_to_wp < 2) 
     {
       drone_hove();
-      delay(5000);
-      drone_land();
+      delay(10000);
+      if (!wp_reached){
+        drone_setwp(lat_home,lon_home);
+        wp_reached = 1 ;
+      }        
+      else drone_land();
     }
     acquired = 0;
   } 
@@ -85,9 +89,9 @@ void pilot_setup()
   Serial.print("\n\n\n\n\n$QUIT\n\n");
   delay(1000);
   Serial.print("cat - | ./data/video/arpilot\n");      //no more USB, binary file in /data/video folder
-  delay(3000);
+  delay(4000);
   Serial.print("cat - | ./data/video/arpilot\n");      // Send it once more for safety
-  delay(1000);
+  delay(4000);
 
   /*Limits: max_yaw 2.50
              max_vz  700
@@ -125,10 +129,10 @@ void drone_sendpos()
 //  Serial.print("\n");
 }
 
-void drone_setwp(float longitude, float latitude)
+void drone_setwp(float latitude, float longitude)
 {
-  lat_wp = longitude;
-  lon_wp = latitude;
+  lat_wp = latitude;
+  lon_wp = longitude;
 }
 
 float calculate_distance(float lat_home, float lon_home, float lat_wp, float lon_wp)
@@ -151,7 +155,7 @@ void drone_takeoff()
   delay(6000);
   Serial.print("$CALI\n");
   delay(6000);
-  Serial.print("$ALTI,1500,0\n");
+  Serial.print("$ALTI,3000,1\n");
   delay(3000);
   flag_flying = 1;
 }
@@ -168,12 +172,12 @@ void drone_move(float dist_to_wp)
 {
 //  Serial.print("$ECHO,#");
 //  Serial.println(dist_to_wp);
-  int kp = (-50);
+  int kp = (-10);
   float tpitch = dist_to_wp * kp;
 //  Serial.print("$ECHO,#");
 //  Serial.println(tpitch);
   if(tpitch > 0) tpitch = 0;
-  if(tpitch < -1024) tpitch = -1024;
+  if(tpitch < -200) tpitch = -200;
   
 //  Serial.print("$ECHO,#");
 //  Serial.println(tpitch);
@@ -187,7 +191,7 @@ void drone_move(float dist_to_wp)
   Serial.print(0);
   Serial.print("\n");
   delay(200);
-  Serial.print("$ALTI,1500,0\n");
+  Serial.print("$ALTI,3000,1\n");
 }
 
 void drone_hove()
