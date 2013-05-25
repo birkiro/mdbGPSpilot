3c8e945e11cf2858818e47b96464d72778b90a4b
birkiro <birkiro@gmail.com>
2013-05-25 12:47:01 +0200

    Changed syntax for log file to be matlab friendly

--- MDBPilot_ARM/log_data.c
+++ MDBPilot_ARM/log_data.c
@@ -8,23 +8,23 @@ int i = 1;
 double start_time;
 
 void logdata(void){
-	while(i>0){
+	while( i > 0 ){ // Only do this once
 		gettimeofday(&ts, NULL);
 		start_time = ts.tv_sec+(ts.tv_usec/1000000.0);
 		i--;
 	}
 	gettimeofday(&ts, NULL);
 	double dTime1 = ts.tv_sec+(ts.tv_usec/1000000.0);
-	fprintf(f, "VX:%f; VY:%f; VZ:%f; ALT:%d; PSI:%f; PHI:%f; THETA:%f; LON:%f; LAT:%f; TIME:%f\n",
-																		navdata_unpacked.navdata_demo.vx,
-																		navdata_unpacked.navdata_demo.vy,
-																		navdata_unpacked.navdata_demo.vz,
-																		navdata_unpacked.navdata_demo.altitude,
-																		navdata_unpacked.navdata_demo.psi,
-																		navdata_unpacked.navdata_demo.phi,
-																		navdata_unpacked.navdata_demo.theta,
-																		pos_lon,
-																		pos_lat,
-																		dTime1-start_time);
-
-}
+	fprintf(f, "%f; %f; %f; %d; %f; %f; %f; %f; %f; %f; %d\n",
+						navdata_unpacked.navdata_demo.vx,
+						navdata_unpacked.navdata_demo.vy,
+						navdata_unpacked.navdata_demo.vz,
+						navdata_unpacked.navdata_demo.altitude,
+						navdata_unpacked.navdata_demo.psi,
+						navdata_unpacked.navdata_demo.phi,
+						navdata_unpacked.navdata_demo.theta,
+						pos_lat,
+						pos_lon,
+						dTime1-start_time,
+						navdata_unpacked.navdata_demo.vbat_flying_percentage );
+} // vx, vy, vz, alt, psi, phi, theta, lat, lon, time, batt
